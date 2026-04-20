317d6bcabb760dfd91c03c210a16180808bb534d
immanuelushindi-cmd <immanuelushindi@gmail.com>
2026-04-20 11:39:37 +0300

    update

---
+++ app/src/main/java/com/immanuel/sokohub/models/User.kt
@@ -0,0 +1,9 @@
+package com.immanuel.sokohub.models
+
+data class User(
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val uid: String = "",
+    val role: String = "user"
+)
